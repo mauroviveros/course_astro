58e7178b668c31b10b04c3d3e70c67ec77bf96bf
Mauro Daniel viveros <maurod.viveros@gmail.com>
2025-10-19 14:48:03 +0200

    34. Controles de la paginación

--- sections/02-pokemon-static/astro.config.mjs
+++ sections/02-pokemon-static/astro.config.mjs
@@ -4,6 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+    redirects: {
+        "/": "/pokemon/1",
+        "/pokemon": "/pokemon/1",
+    },
     vite: {
         plugins: [tailwindcss()],
     },
