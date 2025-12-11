f9c9609da0569a5ee9bda6e873507ac053ea6940
Mauro Daniel viveros <maurod.viveros@gmail.com>
2025-12-11 00:10:26 -0300

    feat: :sparkles: 46. Astro Icon

--- sections/02-pokemon-static/astro.config.mjs
+++ sections/02-pokemon-static/astro.config.mjs
@@ -2,12 +2,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-    redirects: {
-        "/pokemons": "/pokemons/1",
-    },
-    vite: {
-        plugins: [tailwindcss()],
-    },
+  redirects: {
+      "/pokemons": "/pokemons/1",
+  },
+  vite: {
+      plugins: [tailwindcss()],
+  },
+  integrations: [icon()],
 });
