1e6b375c7db2d144c5720972344890f6c61caca7
Mauro Daniel viveros <maurod.viveros@gmail.com>
2026-03-24 20:41:07 -0300

    feat: ✨ 67. Mostrar entradas de blog - Colecciones

--- sections/03-blog/src/content.config.ts
+++ sections/03-blog/src/content.config.ts
@@ -1,12 +1,14 @@
+import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
     type: "content",
+    // loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
     schema: z.object({
         title: z.string(),
         date: z.date(),
         description: z.string(),
-        image: z.string().url(),
+        image: z.string(),
 
         // relacion
         author: z.string(),
