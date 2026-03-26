b50386ff6705fe1679609f684bd85c8ec8a75663
Mauro Daniel viveros <maurod.viveros@gmail.com>
2026-03-26 11:20:35 -0300

    feat: ✨ 98. Imagen y contenido en el News Feed

--- sections/03-blog/src/pages/rss.xml.ts
+++ sections/03-blog/src/pages/rss.xml.ts
@@ -1,19 +1,38 @@
 import rss from '@astrojs/rss';
 import type { APIRoute } from "astro";
 import { getCollection } from 'astro:content';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
 
-export const GET = (async (context) => {
+const parser = new MarkdownIt();
+
+export const GET = (async ({ site }) => {
 	const posts = await getCollection('blog');
 	return rss({
 		// stylesheet: '/rss/styles.xsl',
+		xmlns: {
+			media: 'http://search.yahoo.com/mrss/',
+		},
     title: 'Mauro’s Blog',
     description: 'Un simple blog construido con Astro y Tailwind',
-    site: context.site ?? '',
-    items: posts.map(({ data: { title, description, date }, slug }) => (({
+    site: site ?? '',
+    items: posts.map(({ data: { title, description, date, image }, slug, render, body }) => (({
 			title,
 			description,
 			link: `/posts/${slug}`,
 			pubDate: new Date(date),
+			content: sanitizeHtml(parser.render(body), {
+				allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+			}),
+			customData: `
+				<media:content
+					type="image/${image.format === 'jpg' ? 'jpeg' : 'png'}"
+					width="${image.width}"
+					height="${image.height}"
+					medium="image"
+					url="${site + image.src}"
+				/>
+			`,
     }))),
     // (optional) inject custom xml
     customData: `<language>es-AR</language>`,
