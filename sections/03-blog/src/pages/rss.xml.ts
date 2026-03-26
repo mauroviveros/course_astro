e4bb4012edf4f7fa1705d529fbcf98b3bbb81f44
Mauro Daniel viveros <maurod.viveros@gmail.com>
2026-03-26 10:53:14 -0300

    feat: ✨ 95. RSS Feed - Astro

--- sections/03-blog/src/pages/rss.xml.ts
+++ sections/03-blog/src/pages/rss.xml.ts
@@ -1,5 +1,21 @@
+import rss from '@astrojs/rss';
 import type { APIRoute } from "astro";
+import { getCollection } from 'astro:content';
 
-export const GET = (async ({ params, request }) => {
-	return new Response(JSON.stringify({path: new URL(request.url).pathname}));
+export const GET = (async (context) => {
+	const posts = await getCollection('blog');
+	return rss({
+		// stylesheet: '/rss/styles.xsl',
+    title: 'Mauro’s Blog',
+    description: 'Un simple blog construido con Astro y Tailwind',
+    site: context.site ?? '',
+    items: posts.map(({ data: { title, description, date }, slug }) => (({
+			title,
+			description,
+			link: `/posts/${slug}`,
+			pubDate: new Date(date),
+    }))),
+    // (optional) inject custom xml
+    customData: `<language>es-AR</language>`,
+	});
 }) satisfies APIRoute;
